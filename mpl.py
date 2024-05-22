039322223e0e56b6542b859d4703b64f162ec8d6
Gael Melo <104900699+gmmelo@users.noreply.github.com>
2024-05-22 17:20:53 -0300

    add depth calculation and coloring

--- mpl.py
+++ mpl.py
@@ -23,12 +23,14 @@ def pinhole_projection(point_3d):
     # Project the 3D point onto the image plane
     projection_x = (focal_length * point_3d[0]) / point_3d[2]
     projection_y = (focal_length * point_3d[1]) / point_3d[2]
+    projection_h = np.sqrt(point_3d[0] ** 2 + point_3d[1] ** 2)
     
     # Convert to pixel coordinates
     pixel_x = (projection_x + sensor_width / 2)
     pixel_y = (sensor_height / 2 - projection_y)
+    pixel_depth = np.sqrt(projection_h ** 2 + point_3d[2] ** 2)
     
-    return np.array([pixel_x, pixel_y]) 
+    return np.array([pixel_x, pixel_y, pixel_depth]) 
 
 def main():
     # Define the 3D point and camera parameters
@@ -38,19 +40,23 @@ def main():
     # Rasterize the points and save them to a list
     x_list = []
     y_list = []
+    depth_list = []
 
+    # Create progress markers
     quarter_length = int(len(point_array)/4)
     half_length = int(len(point_array)/2)
     three_quarter_length = int(3*len(point_array)/4)
+
     for index, world_point in enumerate(point_array):
         raster_point = pinhole_projection(world_point)
         x_list.append(raster_point[0])
         y_list.append(raster_point[1])
+        depth_list.append(raster_point[2])
         if (index == quarter_length or index == half_length or index == three_quarter_length):
             print("point " , index , " successfully processed.")
 
     # Draw them as an image
-    plt.scatter(x_list, y_list)
+    plt.scatter(x_list, y_list, c = depth_list)
     plt.show()
 
 if __name__ == "__main__":
