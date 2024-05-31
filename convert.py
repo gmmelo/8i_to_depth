ccf49224f59a0211118aaa54ddfe85b7d2b18e6d
Gael Melo <104900699+gmmelo@users.noreply.github.com>
2024-05-31 01:35:49 -0300

    convert to matrix-based model

--- convert.py
+++ convert.py
@@ -2,34 +2,33 @@ import numpy as np
 from PIL import Image, ImageDraw
 import open3d as o3d
 
+img_width_cm = 0.24
+img_height_cm = 0.24
+focal_length_cm = 0.1
+img_width_pixels = 1024
+img_height_pixels = 1024
+physical_to_pixels = img_width_pixels / img_width_cm
+
 def pinhole_projection(point_3d):
     """
     Project a 3D point onto a 2D image plane using the pinhole camera model.
     
-    Args:
+    Arg:
     - point_3d: A numpy array representing the 3D point in Cartesian coordinates (x, y, z).
-    - camera_params: A dictionary containing camera parameters:
-        - 'focal_length': Focal length of the camera.
-        - 'sensor_width': Width of the image sensor.
-        - 'sensor_height': Height of the image sensor.
         
     Returns:
     - A numpy array representing the 2D projection of the 3D point on the image plane.
     """
-    focal_length = 10
-    sensor_width = 36
-    sensor_height = 24
-    camera_distance = 500
     
     # Project the 3D point onto the image plane
-    projection_x = (focal_length * point_3d[0]) / (point_3d[2] + camera_distance)
-    projection_y = (focal_length * point_3d[1]) / (point_3d[2] + camera_distance)
-    projection_h = np.sqrt(point_3d[0] ** 2 + point_3d[1] ** 2)
+    projection_x = (focal_length_cm * point_3d[0][0]) / (point_3d[2][0])
+    projection_y = (focal_length_cm * point_3d[1][0]) / (point_3d[2][0])
+    projection_h = np.sqrt(point_3d[0][0] ** 2 + point_3d[1][0] ** 2)
     
     # Convert to pixel coordinates
-    pixel_x = (projection_x + sensor_width / 2)
-    pixel_y = (sensor_height / 2 - projection_y)
-    pixel_depth = np.sqrt(projection_h ** 2 + point_3d[2] ** 2)
+    pixel_x = (projection_x + img_width_cm / 2)
+    pixel_y = (img_height_cm / 2 - projection_y)
+    pixel_depth = np.sqrt(projection_h ** 2 + point_3d[2][0] ** 2)
     
     return np.array([pixel_x, pixel_y, pixel_depth]) 
 
@@ -39,9 +38,18 @@ def main():
     point_array = np.asarray(point_load.points)   # Example 3D point
     
     # Rasterize the points and save them to a list
-    x_list = []
-    y_list = []
-    depth_list = []
+    point_count = point_array.shape[0]
+    x_list = np.empty(point_count)
+    y_list = np.empty(point_count)
+    depth_list = np.empty(point_count)
+    
+    # Create the extrinsic matrix
+    camera_x = 0
+    camera_y = -500
+    camera_z = 400
+    world_to_camera = np.array([[1, 0, 0, camera_x],
+                                [0, 1, 0, camera_y],
+                                [0, 0, 1, camera_z]])
 
     # Create progress markers
     quarter_length = int(len(point_array)/4)
@@ -49,29 +57,32 @@ def main():
     three_quarter_length = int(3*len(point_array)/4)
 
     for index, world_point in enumerate(point_array):
-        raster_point = pinhole_projection(world_point)
-        x_list.append(raster_point[0])
-        y_list.append(raster_point[1])
-        depth_list.append(raster_point[2])
+        world_point_homogeneous = np.array([[world_point[0]],
+                                            [world_point[1]],
+                                            [world_point[2]],
+                                            [1]])
+        camera_point = world_to_camera @ world_point_homogeneous
+        raster_point = pinhole_projection(camera_point)
+        x_list[index] = raster_point[0]
+        y_list[index] = raster_point[1]
+        depth_list[index] = raster_point[2]
         if (index == quarter_length or index == half_length or index == three_quarter_length):
             print("point " , index , " successfully processed.")
 
-    img_width = 1024
-    img_height = 1024
-    scale_factor = 40
-    color_factor = 0.3
-    x_offset = -400
-    y_offset = 300
+    
+    depth_matrix = np.zeros((img_height_pixels, img_width_pixels, 3), np.uint8)
 
-    depth_matrix = np.zeros((img_height, img_width, 3), np.uint8) # 1 because grayscale
+    depth_max = max(depth_list)
+    depth_min = min(depth_list)
+    depth_delta = depth_max - depth_min
 
     for i in range(len(depth_list)):
-        x_position = x_list[i] * scale_factor + x_offset
-        y_position = y_list[i] * scale_factor + y_offset
-        color = np.clip(depth_list[i] * color_factor, 0, 255)
-        if ((x_position < img_width and x_position > 0) and (y_position < img_height and y_position > 0)):
+        x_position = x_list[i] * physical_to_pixels
+        y_position = y_list[i] * physical_to_pixels
+        color = 255 * (1 - (depth_list[i] - depth_min) / (depth_delta)) # normalize, scale to 0-255, and invert
+        if ((x_position < img_width_pixels and x_position >= 0) and (y_position < img_height_pixels and y_position >= 0)):
             if depth_matrix[int(y_position), int(x_position), 0] < color:
-                depth_matrix[int(y_position), int(x_position)] = [color, color, color]
+                depth_matrix[int(y_position), int(x_position)] = color
 
 
     # Draw them as an image
