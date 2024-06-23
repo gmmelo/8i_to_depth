6873629d12a0cacc147faba167bac8ca0f020978
Gael Melo <104900699+gmmelo@users.noreply.github.com>
2024-06-23 09:31:34 -0300

    Add working color capture

--- capture.py
+++ capture.py
@@ -42,46 +42,42 @@ def main():
         color_camera_extrinsic_matrix[i] = extrinsic_matrix(0, -600, 1200, 0, current_z_rotation, 0) # Get a 360 view from <camera_count> cameras
         print(f"[DEBUG] Camera[{i}] extrinsic matrix:\n", depth_camera_extrinsic_matrix[i])
 
-    image_matrix_low = np.zeros((camera_count, depth_img_height_pixels, depth_img_width_pixels, 3), np.uint8) # to png
-    image_matrix_high = np.zeros((camera_count, depth_img_height_pixels, depth_img_width_pixels, 3), np.uint8) # to png
-    depth_matrix = np.zeros((camera_count, depth_img_height_pixels, depth_img_width_pixels), np.float16) # to csv
+    depth_matrix_low = np.zeros((camera_count, depth_img_height_pixels, depth_img_width_pixels, 3), np.uint8) # to png
+    depth_matrix_high = np.zeros((camera_count, depth_img_height_pixels, depth_img_width_pixels, 3), np.uint8) # to png
+    depth_matrix_float16 = np.zeros((camera_count, depth_img_height_pixels, depth_img_width_pixels), np.float16) # to csv
     color_matrix = np.zeros((camera_count, color_img_height_pixels, color_img_width_pixels, 3), np.uint8) # to png
 
-    depth_max = np.empty((camera_count))
-    depth_min = np.empty((camera_count))
-    depth_delta = np.empty((camera_count))
+    screen_point_depth_array = rasterize_points_to_matrix(camera_count, depth_camera_extrinsic_matrix, point_coordinate_array)
+    screen_point_color_array = rasterize_points_to_matrix(camera_count, color_camera_extrinsic_matrix, point_coordinate_array, point_color_array)
 
-    screen_point_depth_array = rasterize_points_to_matrix(camera_count, point_coordinate_array, depth_camera_extrinsic_matrix)
-
-    for i in range(camera_count):
-        depth_max[i] = max(screen_point_depth_array[i][2])
-        depth_min[i] = min(screen_point_depth_array[i][2])
-        depth_delta[i] = depth_max[i] - depth_min[i]
-
-    # Paint the image
+    # Paint the images
     for i in range(camera_count):
         for j in range(point_count):
             x_position = int(screen_point_depth_array[i][0][j] * physical_to_pixels_depth)
             y_position = int(screen_point_depth_array[i][1][j] * physical_to_pixels_depth)
-            depth = screen_point_depth_array[i][2][j]
-            color_low, color_high = float16_to_two_int8(depth)
+            depth_float16 = screen_point_depth_array[i][2][j]
+            depth_low, depth_high = float16_to_two_int8(depth_float16)
             if ((x_position < depth_img_width_pixels and x_position >= 0) and (y_position < depth_img_height_pixels and y_position >= 0)):
                 # Check if current point is closer than the one currently at the pixel or if the pixel is blank
-                if (depth_matrix[i][y_position][x_position] > depth or abs(depth_matrix[i][y_position][x_position]) < 0.1): 
-                    image_matrix_low[i, y_position, x_position, 2] = color_low
-                    image_matrix_high[i, y_position, x_position, 2] = color_high
-                    depth_matrix[i, y_position, x_position] = depth
+                if (depth_matrix_float16[i][y_position][x_position] > depth_float16 or abs(depth_matrix_float16[i][y_position][x_position]) < 0.1): 
+                    depth_matrix_low[i, y_position, x_position, 2] = depth_low
+                    depth_matrix_high[i, y_position, x_position, 2] = depth_high
+                    depth_matrix_float16[i, y_position, x_position] = depth_float16
+
+                    color_matrix[i, y_position, x_position] = np.asarray([screen_point_color_array[i][2][j], screen_point_color_array[i][3][j], screen_point_color_array[i][4][j]])
 
     # Save raw depth as csv
     for i in range(camera_count):
-        np.savetxt(f"depth_matrix_{i}.csv", depth_matrix[i], delimiter=",", newline="\n")
+        np.savetxt(f"depth_matrix_float16_{i}.csv", depth_matrix_float16[i], delimiter=",", newline="\n")
         np.savetxt(f"depth_camera_extrinsic_matrix_{i}.csv", depth_camera_extrinsic_matrix[i], delimiter=",", newline="\n")
-        img_low = Image.fromarray(image_matrix_low[i])
-        img_high = Image.fromarray(image_matrix_high[i])
-        img_low.save(f"color_visualization_low_{i}.png")
-        img_high.save(f"color_visualization_high_{i}.png")
+        img_low = Image.fromarray(depth_matrix_low[i])
+        img_high = Image.fromarray(depth_matrix_high[i])
+        img_color = Image.fromarray(color_matrix[i])
+        img_low.save(f"depth_visualization_low_{i}.png")
+        img_high.save(f"depth_visualization_high_{i}.png")
+        img_color.save(f"color_visualization_{i}.png")
 
-def rasterize_points_to_matrix(camera_count, point_coordinate_array, depth_camera_extrinsic_matrix):
+def rasterize_points_to_matrix(camera_count, depth_camera_extrinsic_matrix, point_coordinate_array, point_color_array=np.empty((0, 0))):
     point_count = point_coordinate_array.shape[0]
     # Create progress markers
     marker_counter = 4
@@ -89,8 +85,20 @@ def rasterize_points_to_matrix(camera_count, point_coordinate_array, depth_camer
     marker_index = 0
     print(progress_markers)
 
-    screen_point_depth_array = np.empty((camera_count, 3, point_count), np.float16) # For each camera, store x, y, and distance of each point in screen cm coordinates
-    # Loop through point cloud and add rasterized points to a list
+    if (point_color_array.shape[0] != 0): # True if color, false if depth capture
+        color = True
+    else:
+        color = False
+
+    print(color)
+    print(point_color_array)
+
+    if (color == True):
+        screen_point_array = np.empty((camera_count, 5, point_count), np.uint8) # For each camera, store x, y, and rgb info
+    else:
+        screen_point_array = np.empty((camera_count, 3, point_count), np.float16) # For each camera, store x, y, and distance of each point in screen cm coordinates
+
+    # Loop through point cloud and add rasterized points to an array
     for camera_index in range(camera_count):
         for point_index, world_point in enumerate(point_coordinate_array):
             world_point_homogeneous = np.array([[world_point[0]],
@@ -98,10 +106,20 @@ def rasterize_points_to_matrix(camera_count, point_coordinate_array, depth_camer
                                                 [world_point[2]],
                                                 [1]])
             camera_point = depth_camera_extrinsic_matrix[camera_index] @ world_point_homogeneous
-            raster_point = pinhole_projection(camera_point)
-            screen_point_depth_array[camera_index][0][point_index] = raster_point[0] # Screen X (cm)
-            screen_point_depth_array[camera_index][1][point_index] = raster_point[1] # Screen Y (cm)
-            screen_point_depth_array[camera_index][2][point_index] = raster_point[2] # Point distance (cm)
+
+            if (color == True):
+                raster_point = pinhole_projection(camera_point, point_color_array[point_index])
+                screen_point_array[camera_index][0][point_index] = raster_point[0] # Screen X (cm)
+                screen_point_array[camera_index][1][point_index] = raster_point[1] # Screen Y (cm)
+                screen_point_array[camera_index][2][point_index] = raster_point[2] # Point R
+                screen_point_array[camera_index][3][point_index] = raster_point[3] # Point G
+                screen_point_array[camera_index][4][point_index] = raster_point[4] # Point B
+            else:
+                raster_point = pinhole_projection(camera_point)
+                screen_point_array[camera_index][0][point_index] = raster_point[0] # Screen X (cm)
+                screen_point_array[camera_index][1][point_index] = raster_point[1] # Screen Y (cm)
+                screen_point_array[camera_index][2][point_index] = raster_point[2] # Point distance (cm)
+            
             if (point_index == progress_markers[marker_index]):
                 counter_percentage = (marker_index + 1) / (marker_counter + 1) * 100
                 print(f"point {point_index} successfully processed for camera {camera_index}. {counter_percentage}% done")
@@ -109,7 +127,7 @@ def rasterize_points_to_matrix(camera_count, point_coordinate_array, depth_camer
                 marker_index %= marker_counter
         print(f"Every point has been successfully processed for camera {camera_index}. 100% done")
 
-    return screen_point_depth_array
+    return screen_point_array
 
 def create_progress_markers(marker_count, total_to_process):
     progress_markers = np.empty((marker_count))
@@ -159,7 +177,7 @@ def extrinsic_matrix(camera_position_x, camera_position_y, camera_position_z, ca
 
     return depth_camera_extrinsic_matrix
 
-def pinhole_projection(point_3d):
+def pinhole_projection(point_3d, rgb_info=np.empty((0,0))):
     """
     Project a 3D point onto a 2D image plane using the pinhole camera model.
     
@@ -173,14 +191,24 @@ def pinhole_projection(point_3d):
     # Project the 3D point onto the image plane
     projection_x = -(focal_length_depth_cm * point_3d[0][0]) / (point_3d[2][0]) # Invert signal because of projection properties
     projection_y = -(focal_length_depth_cm * point_3d[1][0]) / (point_3d[2][0])
-    projection_h = np.sqrt(point_3d[0][0] ** 2 + point_3d[1][0] ** 2)
-    
     # Convert to pixel coordinates
     pixel_x = (projection_x + depth_img_width_cm / 2)
     pixel_y = (projection_y + depth_img_height_cm / 2)
-    pixel_depth = np.sqrt(projection_h ** 2 + point_3d[2][0] ** 2)
-    
-    return np.array([pixel_x, pixel_y, pixel_depth]) 
+
+    if(rgb_info.shape[0] == 0):
+        projection_h = np.sqrt(point_3d[0][0] ** 2 + point_3d[1][0] ** 2)
+        pixel_depth = np.sqrt(projection_h ** 2 + point_3d[2][0] ** 2)
+        return np.array([pixel_x, pixel_y, pixel_depth])
+
+    else:
+        red, green, blue = rgb_info[0], rgb_info[1], rgb_info[2]
+        # Necessary because color values are stored as floats ranging from 0 to 1
+        red *= 255
+        green *= 255
+        blue *= 255
+        return np.array([pixel_x, pixel_y, red, green, blue], np.uint8)
+            
+
 
 if __name__ == "__main__":
     main()
